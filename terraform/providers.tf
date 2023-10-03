f5c37ea189dfefcfd63d7426206d8c33ede707bd
Daniel García Rubio <dgarciarubio.Software@gmail.com>
2023-10-04 01:56:18 +0200

    Fix terraform authentication

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,11 +8,29 @@ terraform {
     }
   }
   backend "azurerm" {
-    container_name = "tfstate"
-    key            = "terraform.tfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
+
+  subscription_id = var.azurerm_subscription_id
+  tenant_id       = var.azurerm_tenant_id
+  client_id       = var.azurerm_client_id
+  client_secret   = var.azurerm_client_secret
+}
+variable "azurerm_subscription_id" {
+  type    = string
+}
+variable "azurerm_tenant_id" {
+  type    = string
+}
+variable "azurerm_client_id" {
+  type    = string
+}
+variable "azurerm_client_secret" {
+  type    = string
+  sensitive = true
 }
