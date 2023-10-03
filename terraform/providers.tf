babe0929a1d7eab78aebdd993f821fe16676b2d5
Daniel García Rubio <dgarciarubio.Software@gmail.com>
2023-10-04 00:52:49 +0200

    Update github actions with terraform init.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">=0.12"
+  required_version = "~>1.5"
 
   required_providers {
     azurerm = {
@@ -7,7 +7,10 @@ terraform {
       version = "~>3.0"
     }
   }
-  backend "azurerm" {}
+  backend "azurerm" {
+    container_name = "tfstate"
+    key            = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
