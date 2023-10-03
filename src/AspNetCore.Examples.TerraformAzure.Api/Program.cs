b4055e7aeed9593512560128d65cf02e082f9cc9
Daniel García Rubio <dgarciarubio.Software@gmail.com>
2023-10-03 22:22:48 +0200

    Remove https settings.

--- src/AspNetCore.Examples.TerraformAzure.Api/Program.cs
+++ src/AspNetCore.Examples.TerraformAzure.Api/Program.cs
@@ -14,8 +14,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
