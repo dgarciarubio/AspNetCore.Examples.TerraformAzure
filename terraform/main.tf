94b14becc0782482c710d68723d6333215c7a6a9
Daniel García Rubio <dgarciarubio.Software@gmail.com>
2023-10-05 20:58:32 +0200

    Fix dotnet application deployment.

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,6 +67,9 @@ resource "azurerm_linux_web_app" "app" {
 
   site_config {
     always_on = false
+    application_stack {
+      dotnet_version = "7.0"
+    }
   }
 }
 variable "linux_web_app_name" {
