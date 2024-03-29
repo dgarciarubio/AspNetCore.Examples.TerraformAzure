232668e29c23761e7e43a3c2f8795e0ebb827498
Daniel García Rubio <dgarciarubio.Software@gmail.com>
2024-03-29 17:49:25 +0100

    Add container registry and docker web app.

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,13 +17,16 @@ resource "azurerm_storage_account" "st" {
   location                         = azurerm_resource_group.rg.location
   account_tier                     = var.storage_account_tier
   account_replication_type         = var.storage_account_replication_type
+  access_tier                      = var.storage_account_access_tier
   allow_nested_items_to_be_public  = false
   cross_tenant_replication_enabled = false
   lifecycle {
     prevent_destroy = true
   }
 }
-variable "storage_account_name" { type = string }
+variable "storage_account_name" { 
+  type = string 
+}
 variable "storage_account_tier" {
   type    = string
   default = "Standard"
@@ -31,6 +34,10 @@ variable "storage_account_tier" {
 variable "storage_account_replication_type" {
   type    = string
   default = "LRS"
+}
+variable "storage_account_access_tier" {
+  type    = string
+  default = "Hot"
 }
 
 resource "azurerm_storage_container" "tfstate" {
@@ -40,6 +47,20 @@ resource "azurerm_storage_container" "tfstate" {
   lifecycle {
     prevent_destroy = true
   }
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                             = var.container_registry_name
+  resource_group_name              = azurerm_resource_group.rg.name
+  location                         = azurerm_resource_group.rg.location
+  sku                              = var.container_registry_sku
+}
+variable "container_registry_name" { 
+  type = string 
+}
+variable "container_registry_sku" {
+  type    = string
+  default = "Basic"
 }
 
 resource "azurerm_service_plan" "sp" {
@@ -75,4 +96,28 @@ resource "azurerm_linux_web_app" "app" {
 variable "linux_web_app_name" {
   type    = string
   default = "aspnetcore-examples-terraformazure-linuxwebapp"
+}
+
+resource "azurerm_linux_web_app" "docker_app" {
+  name                = var.docker_web_app_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.sp.id
+  https_only          = true
+
+  webdeploy_publish_basic_authentication_enabled = false
+  ftp_publish_basic_authentication_enabled       = false
+
+  app_settings = {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+  }
+
+  site_config {
+    always_on = false
+    ftps_state = "FtpsOnly"
+  }
+}
+variable "docker_web_app_name" {
+  type    = string
+  default = "aspnetcore-examples-terraformazure-containerwebapp"
 }
