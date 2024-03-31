85042e2d1086b5d91132ca0ce126f5bee24fa81a
Daniel García Rubio <dgarciarubio.Software@gmail.com>
2024-03-31 20:34:38 +0200

    Remove unneeded packages.

--- src/AspNetCore.Examples.TerraformAzure.Api/Program.cs
+++ src/AspNetCore.Examples.TerraformAzure.Api/Program.cs
@@ -1,18 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 var summaries = new[]
 {
@@ -30,9 +18,7 @@ app.MapGet("/weatherforecast", () =>
         ))
         .ToArray();
     return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+});
 
 app.Run();
 
